aba76e51ca6bb21a45208a82c8e98260ff9351d3
Rick <ricleyreis@gmail.com>
2023-08-07 09:36:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+      <a href='' > <span>&lt;rickreisme/&gt;</span> </a>
+      <a href='.sobre' > <span>&lt;Sobre Mim/&gt;</span> </a>
+      <a href='.experiencia' > <span>&lt;Experiência/&gt;</span> </a>
+      <a href='.meus-projetos' > <span>&lt;Meus Projetos/&gt;</span> </a>
+      <a href='.contato' > <span>&lt;Contato/&gt;</span> </a>
       </header>
+
+      <div className='intro'></div>
+
+      <div className='sobre'></div>
+
+      <div className='experiencia'></div>
+
+      <div className='meus-projetos'></div>
+
+      <div className='contato'></div>
+
+      <footer></footer>
     </div>
   );
 }
